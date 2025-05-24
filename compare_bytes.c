3d791448ffa4e19f0d661920b9905d64ebc4554a
Real Yuki <me@reyuki.site>
2025-05-24 07:36:56 +0700

    compare bytes: add strcmp example

--- compare_bytes.c
+++ compare_bytes.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /* so many pointer pointer to pointer of pointer, i love pointer :v */
-int main(void)
+void manual_cmp(void)
 {
 	const char buf[] = "HEAD";
 	const char *methods[] = {"GET", "POST", "HEAD", "PATCH", "PUT", "DELETE", "OPTIONS", "CONNECT", "TRACE", NULL};
@@ -30,6 +30,25 @@ int main(void)
 		printf("%s is a valid HTTP method\n", buf);
 	else
 		printf("%s is not a valid HTTP method\n", buf);
+}
+
+void libc_cmp(void)
+{
+	char test1[] = "GET / HTTP/1.1\r\nHost: google.com\r\nUser-Agent: curl/8.13.0\r\nAccept: */*\r\n\r\n";
+	char test2[] = "\r\n\r\n";
+	for (size_t i = 0; i < strlen(test1); i++) {
+		int result = strcmp(&test1[i], test2);
+		if (result == 0) {
+			printf("reached end of HTTP header\n");
+		}
+		
+	}
+}
+
+int main(void)
+{
+	manual_cmp();
+	libc_cmp();
 
 	return 0;
 }
