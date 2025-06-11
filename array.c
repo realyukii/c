f994f047feb9a110bf553493bbeef1e17b9a93c6
Real Yuki <me@reyuki.site>
2025-06-11 07:18:33 +0700

    flexible array member

---
+++ array.c
@@ -0,0 +1,18 @@
+/* compile and examine it on GDB to see the difference. */
+struct arr {
+	int item;
+	char flex_array[];
+};
+
+struct arr2nd {
+	int item;
+	char *arr;
+};
+
+int main(void)
+{
+	struct arr2nd a;
+	struct arr b;
+
+	return 0;
+}
